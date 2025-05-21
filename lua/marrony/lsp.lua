89847359a0ddf875ddde16dc047bd579b9a865df
Marrony Neris <neris.marrony@gmail.com>
2025-05-21 10:31:16 -0700

    more configs

--- lua/marrony/lsp.lua
+++ lua/marrony/lsp.lua
@@ -1,44 +1,16 @@
 local lsp_zero = require("lsp-zero")
+local mason = require("mason")
+local mason_lspconfig = require('mason-lspconfig')
+local lsp_config = require('lspconfig')
 
-lsp_zero.preset("recommended")
+mason.setup({})
 
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-  })
-})
-
-lsp_zero.on_attach(function(client, buff)
-  local opts = { buffer = buff, remap = false }
-
-  lsp_zero.default_keymaps(opts)
-  --lsp_zero.buffer_autoformat()
-
-  --vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  --vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-  --vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  --vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  --vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  if client.name == 'clangd' then
-    vim.keymap.set("n", "<C-h>", function()
-      vim.lsp.buf.signature_help()
-    end, opts)
-  end
-end)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
+mason_lspconfig.setup({
   ensure_installed = {
     'lua_ls',
-    'tsserver',
+    'ts_ls',
     'eslint',
-    'clangd',
+    --'clangd',
     'cssls',
     'zls',
   },
@@ -47,27 +19,58 @@ require('mason-lspconfig').setup({
   },
 })
 
-lsp_zero.setup_servers({
-  'tsserver',
-  'eslint',
-  'lua_ls',
-  'clangd',
-  'cssls',
-  'zls'
-})
+lsp_zero.preset("recommended")
 
-lsp_zero.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
-  servers = {
-    ['tsserver'] = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-    ['clangd'] = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-    ['lua_ls'] = { 'lua' },
-    ['cssls'] = { 'css', 'scss', 'less' }
-  }
-})
+-- lsp_config.clangd.setup({
+--   cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+--   init_options = {
+--     fallbackFlags = { '-std=c++17' },
+--   },
+-- })
+
+-- local cmp = require("cmp")
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+-- cmp.setup({
+--   mapping = cmp.mapping.preset.insert({
+--     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+--     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+--     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+--     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+--     ["<C-Space>"] = cmp.mapping.complete(),
+--   })
+-- })
+
+lsp_zero.on_attach(function(client, buff)
+  local opts = { buffer = buff, remap = false }
+
+  -- enable jump to definition short cuts
+  lsp_zero.default_keymaps(opts)
+  -- lsp_zero.buffer_autoformat()
+
+  -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  -- vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+  -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  -- if client.name == 'clangd' then
+  --   vim.keymap.set("n", "<C-h>", function()
+  --     vim.lsp.buf.signature_help()
+  --   end, opts)
+  -- end
+end)
+
+-- lsp_zero.format_on_save({
+--   format_opts = {
+--     async = false,
+--     timeout_ms = 10000,
+--   },
+--   servers = {
+--     ['ts_ls'] = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+--     -- ['clangd'] = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+--     ['lua_ls'] = { 'lua' },
+--     ['cssls'] = { 'css', 'scss', 'less' }
+--   }
+-- })
 
 -- require('lspconfig').eslint.setup({
 --   on_attach = function(client, bufnr)
@@ -79,4 +82,13 @@ lsp_zero.format_on_save({
 --   end,
 -- })
 
---lsp_zero.setup()
+-- lsp_zero.setup()
+
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = { "c", "cpp" },
+--   callback = function()
+--     vim.cmd [[syntax match printfSpecifier /%[0-9]*[sdifuxX]/ containedin=string]]
+--     vim.cmd [[highlight printfSpecifier ctermfg=Yellow guifg=Yellow]]
+--   end,
+-- })
+
--- lua/marrony/packer.lua
+++ lua/marrony/packer.lua
@@ -1,3 +1,19 @@
+-- https://github.com/wbthomason/packer.nvim?tab=readme-ov-file#quickstart
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/pcker.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
@@ -13,8 +29,10 @@ return require('packer').startup(function(use)
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use('nvim-treesitter/playground')
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = require("nvim-treesitter.install").update()
+  }
 
   -- colors
   --use({ 'rose-pine/neovim', as = 'rose-pine' })
